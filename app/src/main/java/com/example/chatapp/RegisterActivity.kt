1d450fea4a7bfaadb43124569ef69c74b1fb0a70
denkaty <denisselim2003@gmail.com>
2024-04-30 19:20:00 +0300

    Make LoginActivity.kt load on app run

--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -11,7 +11,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_register)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
